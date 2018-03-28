662e8d786a5cce2b9d0dbe38ab7a059f30d3919d
HuangJiaqi <jiaqi.hjq@alibaba-inc.com>
2018-03-28 21:38:44 +0800

    simple y combinator implemented in python3

---
+++ y-combinator.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+ret = map((lambda f: (lambda x: f(lambda v: (x(x))(v))) (lambda x: f(lambda v: (x(x)(v))))) 
+          (lambda fab: lambda n: 1 if n <= 2 else (fab(n-1) + fab(n-2))), 
+          range(10))
+
+print(list(ret))
