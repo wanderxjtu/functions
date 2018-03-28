815c43d77bba899fb5ceb8819f6de2a9ec201877
HuangJiaqi <jiaqi.hjq@alibaba-inc.com>
2018-03-28 21:54:36 +0800

    remove redundant parenthesis

--- y-combinator.py
+++ y-combinator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-ret = map((lambda f: (lambda x: f(lambda v: (x(x))(v))) (lambda x: f(lambda v: (x(x)(v))))) 
+ret = map((lambda f: (lambda x: f(lambda v: x(x)(v))) (lambda x: f(lambda v: x(x)(v)))) 
           (lambda fab: lambda n: 1 if n <= 2 else (fab(n-1) + fab(n-2))), 
           range(10))
 
